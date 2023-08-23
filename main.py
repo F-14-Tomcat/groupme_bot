ee9a2554c33b3139fc9a1ac7763c99e7649fa40e
Wesley <pl450b@localhost.localdomain>
2023-08-22 22:20:26 -0400

    Full functionality, most errors found

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ def send_msg(msg):
 def update_msgs():
     # API call
     msgs = requests.get("https://api.groupme.com/v3/groups/95739422/messages?token=" + ACCESS_TOKEN)
-
+    messages_texts = []
     # If call is a success, parse out the messages and IDs into a list
     if msgs.status_code == 200:
         data = msgs.json()
@@ -44,36 +44,38 @@ def update_msgs():
     
     else:
         print("Failed API call")
+        return(message_texts)
 
 
-# This is the main loop. Every second check for a new message. If it contains "Log: ", record the entry 
+# This is the main loop. Every 3 seconds check for a new message. If it contains "Log: ", record the entry 
 # into the "log.txt" file. Ignore all other messages
 last_id = 0
 while(True):
     # API call to get messages
-    messages = []
-    try:
-        messages = update_msgs()
+    messages = update_msgs()
 
-    if last_id != messages[0][0]:
-        last_id = messages[0][0]
-        parse_check = messages[0][1][0:5]
-        if "log:" in parse_check.lower():
-            # Get date of entry
-            current_date = datetime.now()
-            formatted_date = current_date.strftime("%d %b %Y %H:%M:%S")
+    if messages != None:
+        if last_id != messages[0][0]:
+            last_id = messages[0][0]
+            parse_check = messages[0][1][0:5]
+            if "log:" in parse_check.lower():
+                # Get date of entry
+                current_date = datetime.now()
+                formatted_date = current_date.strftime("%d %b %Y %H:%M:%S")
 
-            # Format full entry
-            full_entry = formatted_date + ", " + messages[0][1][5:] + "\n"
-            print("---- [NEW ENTRY] ----\n" + full_entry + "---------")
-            
-            # Append entry to local file
-            with open("log.csv", "a") as log_file:
-                log_file.write(full_entry)
-            
-            # Send message in chat to confirm that the message was seen
-            text_in = "make a dissapointed comment about the following: " + messages[0][1][5:]
-            chat_completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": text_in}])
-            send_msg(chat_completion.choices[0].message.content)    
+                # Format full entry
+                full_entry = formatted_date + ", " + messages[0][1][5:] + "\n"
+                print("---- [NEW ENTRY] ----\n" + full_entry + "---------")
+                
+                # Append entry to local file
+                with open("log.csv", "a+") as log_file:
+                    log_file.write(full_entry)
+                    log_file.seek(0)
+                    past = log_file.readlines()
+                
+                # Send message in chat to confirm that the message was seen
+                text_in = "given this:\n\n" + str(past[-20:]) + "\n\nmake a quick dissapointed and sarcastic comment about the following: " + messages[0][1][5:]
+                chat_completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": text_in}])
+                send_msg(chat_completion.choices[0].message.content)   
 
     time.sleep(3)
